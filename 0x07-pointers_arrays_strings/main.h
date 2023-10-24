7af2cd790f5ae815186d173d31a240e64d654c73
hibanahed <hiba123nahed@gmail.com>
2023-10-24 16:25:33 -0700

    msg

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s,char b,unsigned int n);
+char *_memset(char *s,char b, unsigned int n);
 int _putchar(char c);
 chat *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s,char c);
